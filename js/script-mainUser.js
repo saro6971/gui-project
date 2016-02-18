5494d05902bb8cf2e0c7d7454abcd137a8578b9e
saro6971 <sam.ronnlund@gmail.com>
2016-02-18 13:11:30 +0100

    Some changes

--- js/script-mainUser.js
+++ js/script-mainUser.js
@@ -1,47 +1,103 @@
 /**
  * Created by Sam on 2016-02-05.
- */
 
+
+$(document).ready(function () {
+$(function(){
+    $("#shoppingCart, #beerListId").sortable({
+        connectWith: ".connect"
+    }).disableSelection();
+});
+$(function(){
+    $("#shoppingCart, #beerListId").sortable({
+        connectWith: ".connect"
+    }).disableSelection();
+});
+
+/*
+/*
+ * By default elements are not allowed to be dropped in other elements, preventDefault prevents this.
+ */
 function allowDrop(ev) {
     ev.preventDefault();
+    console.log("ALLOW DROP");
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+/*
+ * save the index of the element into index
+ *
+ * */
+var elements = [];
+function drag(element, ev) {
+    var index = elements.indexOf(element);
+    if(index == -1){
+        // if not already in the array then we add it
+        elements.push(element);
+        index = elements.length -1;
+    }
+    ev.dataTransfer.setData("index", index);
+    console.log(index);
 }
 
-function drop(ev) {
+/*
+ * Prevent the default of dropping an element(open as a link)
+ * Get the dragged data(getData()) method,
+ * this will return any data that was set to the same type in the setData method.
+ * The set data was the index of the dragged element
+ * append the dragged element into the target element
+ * */
+
+function drop(target, ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var element = elements[event.dataTransfer.getData('index')];
+    // FIXA HÄR
+    var numb = $(".beerItem").text();
+    var pos = numb.indexOf("Price");
+    //var pos = element.indexOf("Price")
+    console.log(pos);
+    target.appendChild(element);
+    /*var data = ev.dataTransfer.getData("text");
+     console.log(data);
+     ev.target.appendChild(document.getElementById(data));
+     */
 }
+
+
+
+
+
+
+
+/*
+* xmlhttprequest is an api to transfer any data from and to clint/server
+* onreadystatechange property contains the eventhandler to be called when thereadystatechange event is fired, readystate 4 = done, status 200 =succesfull request
+* xhttp.responsetext is a string that contains the response,  payload is a property that contains the information
+* */
+
+
 
 function loadItems() {
     var xhttp = new XMLHttpRequest();
+    xhttp.open("GET","http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_get", true);
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             var beers = JSON.parse(xhttp.responseText).payload;
-           //document.getElementById("beer").innerHTML = JSON.stringify(beers);
-           // console.log(JSON.stringify(beers));
             var i;
-            var beerList = ("<div><ul id='beerListId'>");
+            var beerList = ("<div ><ul id='beerListId'>");
             for (i=0; i < beers.length; i++){
                 if (beers[i]["namn"] != ""){
-                    beerList += ("<div class='beerItem' ><li><a href='http://www.google.se'>" + beers[i]["namn"]  + "<div id='priceId'> Price: " + beers[i]["pub_price"] + " £ </div></a></li></div>" );
+                    beerList += ("<div class='beerItem' draggable='true' ondragstart='drag(this,event)' ><a >" + beers[i]["namn"]  + " Price: " + beers[i]["pub_price"] + " £ </a></div>" );
                 }
             }
             beerList += ("</ul></div>")
             document.getElementById("beer").innerHTML = beerList;
-
-
         }
     };
-    xhttp.open("GET","http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_get", true);
+
     xhttp.send();
-
-
 }
 
 loadItems();
 
-
+/*});
+*/
