46f41b7a2b964ad0b9b06c1cf6b4640cd660fb4f
pali3976 <patric.b.lind@gmail.com>
2016-02-19 12:35:17 +0100

    asd
    asd

--- js/script-bartender.js
+++ js/script-bartender.js
@@ -2,3 +2,27 @@
 function logOutMe(){
     window.alert("Du kan föfan inte logga ut");
 }
+
+function clickBeer() {
+    window.alert("Du har tryckt på BÄRKA");
+
+}
+
+function clickBeerInfo() {
+    window.alert("Du har tryckt på info om bärka");
+
+}
+
+function clickCustomer() {
+    window.alert("Du har tryckt på kunder");
+}
+
+function clickTables() {
+    window.alert("Du har tryckt på Bord");
+}
+
+function clickSaleInfo() {
+    window.alert("Du har tryckt på säljinfo");
+
+}
+
