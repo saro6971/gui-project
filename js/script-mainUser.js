57eebbfb59fc3a65d5e38745da02d422ba3db933
saro6971 <sam.ronnlund@gmail.com>
2016-03-02 13:52:33 +0100

    working drag and drop

--- js/script-mainUser.js
+++ js/script-mainUser.js
@@ -2,25 +2,12 @@
  * Created by Sam on 2016-02-05.
 
 
-$(document).ready(function () {
-$(function(){
-    $("#shoppingCart, #beerListId").sortable({
-        connectWith: ".connect"
-    }).disableSelection();
-});
-$(function(){
-    $("#shoppingCart, #beerListId").sortable({
-        connectWith: ".connect"
-    }).disableSelection();
-});
 
-/*
-/*
+
  * By default elements are not allowed to be dropped in other elements, preventDefault prevents this.
  */
 function allowDrop(ev) {
     ev.preventDefault();
-    console.log("ALLOW DROP");
 }
 
 /*
@@ -37,7 +24,7 @@ function drag(element, ev) {
         index = elements.length-1;
     }
     ev.dataTransfer.setData("index", index);
-    console.log(index);
+   // console.log(index);
 }
 
 /*
@@ -54,27 +41,86 @@ function drop(target, ev) {
     //var pos = element.indexOf("Price")
     //console.log($(element).attr('id'));
     var id = $(element).attr('id');
-    var price = $('#'+id+".beerItem a").html();
-    var place = price.indexOf("Price: ");
-    console.log(price);
-    console.log(place);
-    console.log(id);
-    target.appendChild(element);
+    var price = parseInt($('#'+ id +".beerItem").attr('p'));
+    var name = $('#'+ id +".beerItem").attr('name');
+    var quantity = parseInt($('#'+ id +".beerItem").attr('q'));
+    var boughtBeer = {
+        name: name,
+        beer_id: id,
+        price: price,
+        quantity: quantity
+    };
+    addCart(boughtBeer);
+    //target.appendChild(element);
     /*var data = ev.dataTransfer.getData("text");
      console.log(data);
      ev.target.appendChild(document.getElementById(data));
      */
 }
 
-function addCart(id, price, quantity){
-    //if id in shopping cart
-    if(jQuery.inArray(id,shoppingCartList) !== -1){
-        // If id exists, +1 on number of beers, -1 on quantity, att price to total.
 
+function addCart(boughtBeer){
+    for(var item in shoppingCartList){
+        if (shoppingCartList[item].beer_id == boughtBeer.beer_id){
+            //shoppingCartList[item].quantity -=1;
+            shoppingCartList[item].quantity +=1;
+            updateView(shoppingCartList);
+            return;
+        }
     }
-    else{
-        // if id not in array, add new beer, +price on total, -1 quantity, number of beers = 1
+    boughtBeer.quantity+=1;
+    shoppingCartList.push(boughtBeer);
+    updateView(shoppingCartList);
+}
+
+function removeFromCart(boughtBeer){
+    var idBeer = parseInt(boughtBeer.id.substring(6));
+    var index =-1;
+    for(var i = 0; i<shoppingCartList.length; i++){
+        if(shoppingCartList[i].beer_id == idBeer){
+            shoppingCartList[i].quantity-=1;
+            if(shoppingCartList[i].quantity == 0){
+                shoppingCartList.splice(i,1);
+            }
+        }
+        updateView(shoppingCartList);
     }
+}
+
+function addButtonCart(boughtBeer){
+    var idBeer = parseInt(boughtBeer.id.substring(3));
+    for(var item in shoppingCartList){
+        if(shoppingCartList[item].beer_id == idBeer){
+            shoppingCartList[item].quantity +=1;
+        }
+    }
+    updateView(shoppingCartList);
+}
+
+function updateView(shoppingCartList){
+    var buyingBeer ="<div><ul>";
+    for(var item in shoppingCartList){
+        buyingBeer += "<li class='boughtBeer' id=" +'bought'+ shoppingCartList[item].beer_id +" q="+shoppingCartList[item].quantity+" ><span class='shopName'><b>";
+        buyingBeer += shoppingCartList[item].name;
+        buyingBeer += "</b></span><span class='price'>"
+        buyingBeer += shoppingCartList[item].quantity;
+        buyingBeer += " x "+ shoppingCartList[item].price +" = " + shoppingCartList[item].price *shoppingCartList[item].quantity +" SEK";
+        buyingBeer += "</span>   <button class='deleteButton' id=" +'delete' + shoppingCartList[item].beer_id +" type='button' onclick=\"removeFromCart(this)\" ><b>-</b></button>" +
+                                "<button class='addButton' id=" +'add' + shoppingCartList[item].beer_id +" type='button' onclick=\"addButtonCart(this)\" ><b>+</b></button>";
+      }
+    var sum = totalSum();
+    buyingBeer +="</li></ul></div><span class='totalSum'>Total sum: "+ sum +" SEK</span>"
+    document.getElementById("shoppingCart").innerHTML = buyingBeer;
+}
+
+function totalSum(){
+    var sum = 0;
+    for(item in shoppingCartList){
+        var tmpQuant = shoppingCartList[item].quantity;
+        var tmpPrice = shoppingCartList[item].price;
+        sum += tmpPrice * tmpQuant;
+    }
+    return sum;
 }
 
 /*
@@ -82,8 +128,6 @@ function addCart(id, price, quantity){
 * onreadystatechange property contains the eventhandler to be called when thereadystatechange event is fired, readystate 4 = done, status 200 =succesfull request
 * xhttp.responsetext is a string that contains the response,  payload is a property that contains the information
 * */
-
-
 
 function loadItems() {
     var xhttp = new XMLHttpRequest();
@@ -95,8 +139,17 @@ function loadItems() {
             var beerList = ("<div ><ul id='beerListId'>");
             for (i=0; i < beers.length; i++){
                 if (beers[i]["namn"] != ""){
-                    beerList += ("<li id ="+ beers[i]["beer_id" ] + " class='beerItem' draggable='true' ondragstart='drag(this,event)' >"+ beers[i]["namn"] +
-                    "<a class='priceClass'>"   + " Price: " + beers[i]["pub_price"] + " £ </a></li>" );
+                    beerList += ("<li id ="+ beers[i]["beer_id" ]
+                    + " p=" + beers[i]['pub_price']
+                    + " q=0"
+                        // + " q="+beers[i]['count']
+                    + " name=\""+beers[i]['namn'] +" "+ beers[i]['namn2'] + "\""
+                    + " class='beerItem' draggable='true' ondragstart='drag(this,event)' ><span>"
+                    + beers[i]["namn"] +" "+beers[i]["namn2"]+
+                    "</span><span class='price'>  "   +
+                    "  Price: "
+                    + beers[i]["pub_price"] +
+                    " SEK </span></li>" );
                 }
             }
             beerList += ("</ul></div>")
@@ -108,6 +161,3 @@ function loadItems() {
 //keeping track of shopping
 var shoppingCartList = [];
 loadItems();
-
-/*});
-*/
