831b55c0f97321ddcc4698ad42baa85c2d004650
pali3976 <patric.b.lind@gmail.com>
2016-03-03 15:50:35 +0100

    Fixa ID!!!

--- js/script-bartender.js
+++ js/script-bartender.js
@@ -81,6 +81,7 @@ function loadNotifications() {
     xhttp.send();
 }
 
+// ÄNDRA TILL ID OCH HÄMTA SKITEN
 function loadBeerInfo() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET","http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_get", true);
@@ -88,11 +89,17 @@ function loadBeerInfo() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             var beers = JSON.parse(xhttp.responseText).payload;
             var i;
-            var beerList = ("<div ><ul id='beerListId'>");
+            var beerList = ("<div ><ul class = 'beerInfoUl'>");
             for (i=0; i < beers.length; i++){
                 if (beers[i]["namn"] != ""){
-                    beerList += ("<li id =" + beers[i]["beer_id"] + ">" + beers[i]["namn"] + " Price: " + beers[i]["pub_price"] + " kr. Amount: " + beers[i]["count"] +
-                    " ID: " + beers[i]["beer_id"] + "</a></li>" );
+                    beerList += ("<li><p>Name1: " + beers[i]["namn"] + " - Name2: " + beers[i]["namn2"] +
+                        "<button onclick='editBeer( \' " +  beers[i]["namn"] + "\' , \'" + beers[i]["namn2"] +
+                        "\', \'" + beers[i]["count"] + "\', \'" + beers[i]["price"] + "\', \'" + beers[i]["beer_id"] + ")'>" +
+                        "" +
+                        "" +
+                        "Edit</button></p>" +
+                        "Amount: " + beers[i]["count"] + " - Price: " + beers[i]["price"] + "<button onclick='orderBeer=(" +
+                        beers[i]["namn"] + ", " + beers[i]["namn2"] + ", " + beers[i]["count"] + ")'>Order</button></p></li>");
                 }
             }
             beerList += ("</ul></div>")
@@ -100,4 +107,42 @@ function loadBeerInfo() {
         }
     };
     xhttp.send();
+}
+/*
+ <ul class = "beerInfoUl">
+ <li><p>Name1: BärkaNamn1 - Name2: BärkaNamn 2
+ <button onclick="editBeer('nameInfoOne','nameInfoTwo','amountInfo','priceInfo','id')">Ändra</button></p>
+
+ <p>Amount: 24 - Price: 55
+ <button onclick="orderBeer(nameInfoOne,nameInfoTwo,id)">Beställ</button></p>
+
+ </li>
+ </ul>
+
+
+ */
+
+function editBeer(nameOne,nameTwo, amount, price,id){
+    elements = ("<form name='beerInfoInput'' onSubmit='applyEdit()'>" +
+    "<ul id ='temp'> <li>Name1:<input type='text' name='beerInfoInputNameOne' value='" + nameOne +
+    "'></li><li>Name2:<input type='text' name='beerInfoInputNameTwo' value='" + nameTwo +
+    "'></li><li>Amount:<input type='text' name='beerInfoInputAmount' value='" + amount +
+    "'></li>" + "<li>Price:<input type='text' name='beerInfoInputPrice' value='" + price +
+    "'></li> </ul><input type='submit' name='editSubmit' value='Ändra'><button onclick ='closeDiv()'>Close</button></form>");
+
+    document.getElementById("lightEdit").innerHTML = elements;
+    document.getElementById('lightEdit').style.display='block';document.getElementById('fadeEdit').style.display='block';
+}
+
+function closeDiv(){
+    removeElementById('temp');
+    document.getElementById('lightEdit').style.display='none';document.getElementById('fadeEdit').style.display='none';
+}
+ function applyEdit(){
+
+ }
+
+function orderBeer(nameInfoOne,nameInfoTwo,id){
+
+
 }
